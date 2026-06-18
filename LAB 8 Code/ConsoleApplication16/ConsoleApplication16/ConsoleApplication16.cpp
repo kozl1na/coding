ff7b1098e0a86d4cc8dcbd5714133640b8f539fe
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:18:34 +0700

    Laboratory Work #8

---
+++ LAB 8 Code/ConsoleApplication16/ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+void p(double x[], double y[], int size) {
+    double summa = 0;
+    double m = 0;
+    for (int i = 0; i < size; i++) {
+        summa += x[i];
+    }
+    m = summa / size;
+    for (int i = 0; i < size; i++) {
+        y[i] = x[i] - m;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int size;
+    cout << "Размерность массива: ";
+    cin >> size;
+
+    double x[1000], y[1000];
+    cout << "Заполняем массив x: ";
+    for (int i = 0; i < size; i++) {
+        cin >> x[i];
+    }
+
+    p(x, y, size);
+
+    cout << "Результат: ";
+    for (int i = 0; i < size; i++) {
+        cout << y[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+//5.3 4.6 2.8 9.3 7.5
---
+++ LAB 8 Code/ConsoleApplication18/ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool func(int number) {
+	for (int i = 2; i <= sqrt(number); i++) {
+		if (number % i == 0) {
+			return true;
+		}
+	}
+}
+
+
+int main() {
+	setlocale(LC_ALL, "");
+	int massiv[1000];
+	int size;
+	cout << "Размерность массива: ";
+	cin >> size;
+	cout << "Заполнение массива: ";
+	for (int i = 0; i < size; i++) {
+		cin >> massiv[i];
+	}
+	int summa = 0;
+	for (int i = 0; i < size; i++) {
+		if (func(massiv[i])) {
+			summa += massiv[i];
+		}
+	}
+	cout << "Результат: " << summa;
+	return 0;
+}
+
+//4 3 6 7 8
