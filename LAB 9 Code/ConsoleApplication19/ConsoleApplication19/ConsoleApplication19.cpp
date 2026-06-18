7693325609d39e5ff1c2a95f469fb3866bc8ec9d
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:19:43 +0700

    Laboratory Work #9

---
+++ LAB 9 Code/ConsoleApplication19/ConsoleApplication19/ConsoleApplication19.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+struct sport {
+	string surname;
+	int age;
+	float height;
+};
+
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int N;
+	cout << "Кол-во спортсменов: ";
+	cin >> N;
+
+	sport *people = new sport[N];
+
+	for (int i = 0; i < N; i++) {
+		cout << "Введем фамилию, возраст и рост спортсмена: ";
+		cin >> people[i].surname >> people[i].age >> people[i].height;
+	}
+
+	int max_height_index = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (people[i].height > people[max_height_index].height) {
+			max_height_index = i;
+		}
+	}
+
+	cout << "Самый высокий спортсмен - " << people[max_height_index].surname;
+
+	return 0;
+}
+
+//Иванов 27 185
+//Петров 19 190
+//Сидоров 24 187
---
+++ LAB 9 Code/ConsoleApplication21/ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+struct WORKER {
+	string NAME;
+	string POS;
+	int YEAR;
+
+};
+
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	WORKER TABL[10];
+
+	int year_now = 2025;
+
+
+	for (int i = 0; i < 10; i++) {
+		cout << "Введём ФИО работника, должность и год поступления на работу: ";
+		cin >> TABL[i].NAME >> TABL[i].POS >> TABL[i].YEAR;
+	}
+
+	int min_work_experience;
+	cout << "Минимальный стаж работы: ";
+	cin >> min_work_experience;
+
+
+	bool proverka = false;
+
+	for (int i = 0; i < 10; i++) {
+		int work_experience = year_now - TABL[i].YEAR;
+		if (work_experience > min_work_experience) {
+			cout << "ФИО: " << TABL[i].NAME << "\n";
+			proverka = true;
+		}
+	}
+	if (proverka != true) {
+		cout << "Подходящих работников не нашлось.";
+	}
+	return 0;
+}
+
+//Иванов_И.И. Аналитик 2020
+
+//Сидоров_С.С. Разработчик 2019
+
+//Кузнецов_К.К. Маркетолог 2021
+
+//Васильев_В.В. Инженер 2017
+
+//Михайлов_М.М. Дизайнер 2022
+
+//Фёдоров_Ф.Ф. Экономист 2016
+
+//Алексеев_А.А. Логист 2018
+
+//Николаев_Н.Н. Программист 2023
+
+//Орлов_О.О. Финансист 2015
+
+//Романов_Р.Р. Юрист 2019
