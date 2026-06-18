42ca5903e369323df711d3dce8fba3da169b655c
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:12:36 +0700

    Laboratory Work #6

---
+++ LAB 6 Code/ConsoleApplication12/ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string stroka_1;
+	string stroka_2;
+	string answer;
+	cout << "Первая строка: ";
+	cin >> stroka_1;
+	cout << "Вторая строка: ";
+	cin >> stroka_2;
+
+	for (char bykva : stroka_1) {
+		for (char bykva_1 : stroka_2) {
+			if (bykva == bykva_1) { 
+				answer += bykva;
+			}
+		}
+	}
+	if (answer != "") {
+		cout << "Общие буквы: " << answer;
+	}
+	else {
+		cout << "Нет общих букв.";
+	}
+
+	return 0;
+}
---
+++ LAB 6 Code/ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+struct work {
+	string name;
+	char gender;
+	int age;
+	string department;
+};
+
+int main() {
+	SetConsoleCP(1251);         
+	SetConsoleOutputCP(1251);
+
+	int n;
+	cout << "Кол-во работников: ";
+	cin >> n;
+
+	work* rabotnik = new work[n];
+
+	for (int i = 0; i < n; i++) {
+		cout << "\nФИО работника: ";
+		cin >> rabotnik[i].name;
+
+		cout << "Пол(m/f): ";
+		cin >> rabotnik[i].gender;
+
+		cout << "Возраст (полных лет): ";
+		cin >> rabotnik[i].age;
+
+		cout << "Наименование подразделения: ";
+		cin >> rabotnik[i].department;
+	}
+
+	ofstream FIRST_FILE("work.txt");
+	for (int i = 0; i < n; i++) {
+		FIRST_FILE << rabotnik[i].name << " "
+			<< rabotnik[i].gender << " "
+			<< rabotnik[i].age << " "
+			<< rabotnik[i].department << "\n";
+	}
+	FIRST_FILE.close();
+
+	ifstream FILE("work.txt");
+
+	cout << "\nДАННЫЕ ИЗ ФАЙЛА:\n";
+	for (int i = 0; i < n; i++) {
+		FILE >> rabotnik[i].name;
+		FILE >> rabotnik[i].gender;
+		FILE >> rabotnik[i].age;
+		FILE >> rabotnik[i].department;
+
+		cout << "\nФИО работника: " << rabotnik[i].name << endl;
+		cout << "Пол (м/ж): " << rabotnik[i].gender << endl;
+		cout << "Возраст (полных лет): " << rabotnik[i].age << endl;
+		cout << "Наименование подразделения: " << rabotnik[i].department << endl;
+	}
+
+	FILE.close();
+	delete[] rabotnik; 
+	return 0;
+}
+
+//Петров_Иван
+//Маркетинг
+//Сидорова_Анна
+//Бухгалтерия
+//Кузьмин_Павел
+//Разработка
+//Иванова_Ольга
+//Тестирование
+//Смирнов_Алексей
+//Продажи
+
