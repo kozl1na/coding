0cf737a10e1f7f65ab7b79cfa365e262b58fb10b
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:16:20 +0700

    Laboratory Work #7

---
+++ LAB 7 Code/ConsoleApplication14/ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+struct train {
+	string name;
+	char gender;
+	int age;
+	int number_wagon;
+	string category_wagon;
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int n;
+	cout << "Кол-во пассажиров: ";
+	cin >> n;
+
+	train* pas_train = new train[n];
+
+	for (int i = 0; i < n; i++) {
+		cout << "\nФИО пассажира: ";
+		cin >> pas_train[i].name;
+
+		cout << "Пол(m/f): ";
+		cin >> pas_train[i].gender;
+
+		cout << "Возраст (полных лет): ";
+		cin >> pas_train[i].age;
+
+		cout << "Номер вагона: ";
+		cin >> pas_train[i].number_wagon;
+
+		cout << "Категория вагона: ";
+		cin >> pas_train[i].category_wagon;
+	}
+
+	ofstream FIRST_TRAIN_F("work.txt");
+	for (int i = 0; i < n; i++) {
+		FIRST_TRAIN_F << pas_train[i].name << " "
+			<< pas_train[i].gender << " "
+			<< pas_train[i].age << " "
+			<< pas_train[i].number_wagon << " "
+			<< pas_train[i].category_wagon << "\n";
+	}
+	FIRST_TRAIN_F.close();
+
+	int K;
+	cout << "\nПАССАЖИРЫ С НОМЕРОМ ВАГОНА: ";
+	cin >> K;
+
+	ifstream TRAIN_F("work.txt");
+
+	bool proverka = false;
+
+	for (int i = 0; i < n; i++) {
+		if (pas_train[i].number_wagon == K) {
+			proverka = true;
+			cout << "\nФИО пассажира: " << pas_train[i].name << endl;
+			cout << "Пол (m/f): " << pas_train[i].gender << endl;
+			cout << "Возраст (полных лет): " << pas_train[i].age << endl;
+			cout << "Категория вагона: " << pas_train[i].category_wagon << endl;
+		}
+	}
+
+	if (!proverka) {
+		cout << "Нет пассажиров в вагоне с номером " << K << "." << endl;
+	}
+	delete[] pas_train;
+	return 0;
+}
+
+	//Иванов_Иван
+	//Плацкарт
+	//Петров_Петр
+	//Купе
+	//Смирнов_Алексей
+	//Плацкарт
+	//Сидорова_Анна
+	//Купе
+	//Кузьмин_Павел
+	//Купе
---
+++ LAB 7 Code/ConsoleApplication15/ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string s;
+    string word;
+    string last_word;
+    char first_bykva;
+    cout << "Начальная строка S: ";
+    getline(cin, s);
+    for (char bykva : s) {
+        if (bykva != ' ') {
+            word += bykva;
+        }
+        else {
+            if (word != "") {
+                last_word = word;
+                word = "";
+            }
+        }
+    }
+    if (word != "") {
+        last_word = word;
+    }
+
+    first_bykva = last_word[0];
+
+    word = "";
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+        if (s[i] != ' ') {
+            word = s[i] + word;
+        }
+        if (word != "") {
+            if (word[0] == first_bykva) {
+                cout << word << " ";
+                word = "";
+            }
+            else if (word[0] != first_bykva and s[i] == ' ') {
+                word = "";
+            }
+
+        }
+    }
+    if (word != "" && word[0] == first_bykva) {
+        cout << word;
+    }
+    return 0;
+}
+
+
+
+//тунец палка комп туман танец камень томь
