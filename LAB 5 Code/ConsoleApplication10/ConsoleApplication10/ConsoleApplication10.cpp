39880586d555f604827a0f40211591eae55656c3
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:09:47 +0700

    Laboratory Work #5

---
+++ LAB 5 Code/ConsoleApplication10/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int rekurs_func_fib(int n) {
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	else {
+		return rekurs_func_fib(n - 1) + rekurs_func_fib(n - 2);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+	int summa_fib = 0; 
+	int index_fib = 0;
+	int chislo_fib; 
+	while (true) {
+		chislo_fib = rekurs_func_fib(index_fib);
+		if (chislo_fib > 1000) {
+			break;
+		}
+		summa_fib += chislo_fib;
+		index_fib += 1;
+	}
+	cout << "Ответ: " << summa_fib;
+	return 0;
+}
---
+++ LAB 5 Code/ConsoleApplication11/ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int func_rekurs(int massiv[], int index, int n) {
+	if (index == n) {
+		return 1;
+	}
+	else {
+		return massiv[index] * func_rekurs(massiv, index + 1, n);
+	}
+} 
+
+int main() {
+	setlocale(LC_ALL, "");
+	const int time_size = 10000;
+	int massiv[time_size];
+	int n;
+	cout << "Размерность массива: ";
+	cin >> n;
+	cout << "Заполнение массива: ";
+	for (int i = 0; i < n; i++) {
+		cin >> massiv[i];
+	}
+	int answer = func_rekurs(massiv, 0, n);
+	cout << "Ответ: " << answer;
+	return 0;
+}
+
+//1 2 3 4 5 6 7 8 9 3
