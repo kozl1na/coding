32931b1334621d3e3e29b6e7f92da3250dfe1fea
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:05:46 +0700

    Laboratory Work #2

---
+++ LAB 2 Code/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	double a;
+	double b;
+	double c;
+	cout << "Введем три числа : ";
+	cin >> a >> b >> c;
+
+	if (a == b || a == c || b == c) {
+		cout << "Есть пара равных чисел.";
+	} else { 
+		cout << "Нет пар равных чисел.";
+	}
+	return 0;
+}
+
---
+++ LAB 2 Code/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	double x, y;
+	cout << "Введём координаты (x, y): ";
+	cin >> x >> y;
+
+	if (x >= 0 && x <= 1 && y >= 0 && y <= 1) {
+		cout << "Точка принадлежит области.";
+	}
+	else {
+		cout << "Точка не принадлежит области.";
+	}
+	return 0;
+}
