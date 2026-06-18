b0cc30106bf65726e3b50f8429272a8fb594a217
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:07:04 +0700

    Laboratory Work #3

---
+++ LAB 3 Code/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int n, m;
+	int summa = 0;
+	cout << "Введём кол-во цифр m и число n: ";
+	cin >> m >> n;
+	int nachalnoe_n = n;
+
+	for (int i = 0; i < m && n > 0; i++) {
+		summa += n % 10;
+		n /= 10;
+	}
+	
+	cout << "Сумма последних " << m << " числа " << nachalnoe_n << ": " << summa;
+	
+	return 0;
+
+}
---
+++ LAB 3 Code/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	int n;
+	cout << "Введем кол-во действительных чисел (>=3): ";
+	cin >> n;
+
+	if (n < 3) {
+		cout << "Ошибка";
+		return 1;
+	}
+
+	double *hranit = new double[n];
+	cout << "Заполним массив количсетвом " << n << " значений: ";
+	for (int i = 0; i < n; i++) {
+		cin >> hranit[i];
+	}
+	double resh = 1;
+
+	for (int i = 0; i < n - 2; i++) {
+		resh *= (hranit[i] + 2 * hranit[i + 1] + hranit[i + 2]);
+	}
+	cout << "Результат: " << resh;
+
+	return 0;
+}
+
