a7217b0bd7f258505fc7e17d2319657986b54077
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:02:42 +0700

    Laboratory Work #1

---
+++ LAB 1 Code/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "");
+	int age;
+	cout << "Input age: ";
+	cin >> age;
+	cout << "Surname: Козлов" << "\n";
+	cout << "Name: Егор" << "\n";
+	cout << "Number of group: 584-1" << "\n";
+	cout << "Age: " << age << "\n";
+	cout << "Other info: Я люблю пельмени.";
+
+	return 0; 
+}
---
+++ LAB 1 Code/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	float chislo;
+	float res;
+	cout << "Вводим число: ";
+	cin >> chislo;
+	res = chislo * 0.2;
+	cout << "Ответ: " << res;
+	
+	return 0;
+}
