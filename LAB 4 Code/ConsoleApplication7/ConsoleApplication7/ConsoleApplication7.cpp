61e9685076ed07f69306a42ae6bd9cfb1d2a4810
kozl1na <moneyorhoney1@gmail.com>
2026-06-18 20:08:32 +0700

    Laboratory Work #4

---
+++ LAB 4 Code/ConsoleApplication7/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int n;
+    cout << "Введите количество чисел (>=3): ";
+    cin >> n;
+
+    if (n < 3) {
+        cout << "Ошибка";
+        return 1;
+    }
+
+    double x1;
+    double x2;
+    double x3;
+    double result = 1;
+
+    cout << "Вводим три числа: ";
+    cin >> x1 >> x2 >> x3;
+
+    result *= (x1 + 2 * x2 + x3);
+
+    for (int i = 4; i <= n; i++) {
+        
+        double new_chislo;
+        cin >> new_chislo; 
+
+     
+        result *= (x2 + 2 * x3 + new_chislo);
+
+        x1 = x2;
+        x2 = x3;
+        x3 = new_chislo;
+    }
+
+    cout << "Результат: " << result;
+
+    return 0;
+}
---
+++ LAB 4 Code/ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool prostoe(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int summa_prostix(int massiv[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        if (prostoe(massiv[i])) {
+            sum += massiv[i];
+        }
+    }
+    return sum;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    int kolvo_strok, kolvo_stolbec;
+    cout << "Введите количество строк и столбцов: ";
+    cin >> kolvo_strok >> kolvo_stolbec;
+
+    int matrix[100][100];
+    int massiv_prostix[100];
+
+    cout << "Введите элементы матрицы:" << endl;
+    for (int i = 0; i < kolvo_strok; i++) {
+        for (int j = 0; j < kolvo_stolbec; j++) {
+            cin >> matrix[i][j];
+        }
+        massiv_prostix[i] = summa_prostix(matrix[i], kolvo_stolbec);
+    }
+    for (int i = 0; i < kolvo_strok - 1; i++) {
+        for (int j = 0; j < kolvo_strok - i - 1; j++) {
+            if (massiv_prostix[j] < massiv_prostix[j + 1]) {
+                for (int k = 0; k < kolvo_stolbec; k++) {
+                    int copy = matrix[j][k];
+                    matrix[j][k] = matrix[j + 1][k];
+                    matrix[j + 1][k] = copy;
+                }
+                int coppy_summa_prostix = massiv_prostix[j];
+                massiv_prostix[j] = massiv_prostix[j + 1];
+                massiv_prostix[j + 1] = coppy_summa_prostix;
+            }
+        }
+    }
+
+    cout << "Ответ:" << "\n";
+    for (int i = 0; i < kolvo_strok; i++) {
+        for (int j = 0; j < kolvo_stolbec; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+//7 3 6 4
+//9 0 2 5
+//5 2 8 3
